3e275263768744973d4db297233e26b448b0823b
Webskitters <vikashofficial9@gmail.com>
2024-09-27 17:46:05 +0530

    Arcane smart contract

---
+++ AtaSmartContract/Arcane.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Arcane is ERC721, ERC721URIStorage, Ownable {
+    constructor(address initialOwner)
+        ERC721("Arcane", "ATA")
+        Ownable(initialOwner)
+    {}
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
